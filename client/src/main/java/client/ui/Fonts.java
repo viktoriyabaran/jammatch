4f4234b257c5afa92029e3079f658e00325e6616
viktoriyabaran <viktoriabaran83@gmail.com>
2026-06-19 00:49:27 +0300

    Stop logging the font names

--- client/src/main/java/client/ui/Fonts.java
+++ client/src/main/java/client/ui/Fonts.java
@@ -4,9 +4,7 @@ import javafx.scene.text.Font;
 
 public final class Fonts {
     public static void load() {
-        var grotesk = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/SpaceGrotesk-Light.ttf"), 10);
-        var mono    = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/SpaceMono-Regular.ttf"), 10);
-        System.out.println("grotesk -> family=[" + grotesk.getFamily() + "] name=[" + grotesk.getName() + "]");
-        System.out.println("mono    -> family=[" + mono.getFamily() + "] name=[" + mono.getName() + "]");
+        Font.loadFont(Fonts.class.getResourceAsStream("/fonts/SpaceGrotesk-Light.ttf"), 10);
+        Font.loadFont(Fonts.class.getResourceAsStream("/fonts/SpaceMono-Regular.ttf"), 10);
     }
 }
